edef607ac3da7c06d59a8fe62f02bd1f5c3a8646
André <andremessias.az@gmail.com>
2022-07-27 20:17:13 -0300

    add search to app and refactor inital state

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,54 @@
-import {useState} from 'react'
-import {nanoid} from 'nanoid'
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import { NotesList } from "./components/NotesList";
-import './App.css'
+import "./App.css";
+import { Search } from "./components/Search";
+
+const initialState = [
+  {
+    id: nanoid(),
+    text: "This is my first note",
+    date: "15/04/2021",
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note",
+    date: "15/04/2021",
+  },
+  {
+    id: nanoid(),
+    text: "This is my third note",
+    date: "15/04/2021",
+  },
+]
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: 'This is my first note',
-      date: '15/04/2021'
-    },
-    {
-      id: nanoid(),
-      text: 'This is my second note',
-      date: '15/04/2021'
-    },
-    {
-      id: nanoid(),
-      text: 'This is my third note',
-      date: '15/04/2021'
-    }
-  ]);
+  const [notes, setNotes] = useState(initialState);
 
   const addText = (text) => {
     const date = new Date();
     const newNote = {
       id: nanoid(),
       text,
-      date: date.toLocaleDateString()
-    }
-    setNotes(notes => [...notes, newNote])
-  }
+      date: date.toLocaleDateString(),
+    };
+    setNotes((notes) => [...notes, newNote]);
+  };
 
   const deleteNote = (id) => {
-    setNotes(notes.filter(note => note.id !== id))
-  }
+    setNotes(notes.filter((note) => note.id !== id));
+  };
 
   return (
     <div className="container">
-      <NotesList addText={addText} handleDeleteNote={deleteNote} notes={notes}/>
+      <Search />
+      <NotesList
+        addText={addText}
+        handleDeleteNote={deleteNote}
+        notes={notes}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
