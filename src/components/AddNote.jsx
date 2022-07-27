d913648e0474c619b3e30cf7c53d23b104406e0e
André <andremessias.az@gmail.com>
2022-07-27 17:21:00 -0300

    finish addnote style

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,11 +1,11 @@
 export const AddNote = () => {
   return (
-    <div className="note new-note">
-      <textarea cols="10" rows="8" placeholder="Type to add a note">
+    <div className="note new">
+      <textarea cols="10" rows="8" placeholder="Type to add a note...">
       </textarea>
       <div className="note-footer">
         <small>200 remaing</small>
-        <button className="save">save</button>
+        <button className="save-btn">save</button>
       </div>
     </div>
   );
